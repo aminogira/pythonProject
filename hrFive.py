1325010f544753f4f949d6245d848d5f10caf8da
Amila Giragama <aminogira@gmail.com>
2022-11-03 11:58:30 +0530

    day one end

---
+++ hrFive.py
@@ -0,0 +1,8 @@
+if __name__ == '__main__':
+    nm = []
+    sc = []
+    for _ in range(int(input())):
+        name = input()
+        score = float(input())
+        nm.append(name)
+        sc.append(score)
