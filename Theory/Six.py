a4f706a86c756bd2534dbe3986350706ef32b257
Amila Giragama <aminogira@gmail.com>
2022-11-03 21:16:26 +0530

    end 5

---
+++ Theory/Six.py
@@ -0,0 +1,31 @@
+#https://docs.python.org/3.11/tutorial/modules.html
+print('#' * 50)
+print('6. Modules')
+
+import fibo
+fibo.fib(1000)
+print(fibo.fib2(100))
+print(fibo.__name__)
+
+print('assign it to a local name')
+fi = fibo.fib
+fi(500)
+
+
+
+
+print('#' * 50)
+print('6.1. More on Modules')
+
+print('imports names from a module')
+from fibo import fib, fib2
+fib(500)
+
+print('imports  used when utilising')
+from fibo import fib as fibonacci
+fibonacci(500)
+
+
+print('#' * 50)
+print('6.1.1. Executing modules as scripts')
+
--- Theory/TheoryFive02.py
+++ Theory/TheoryFive02.py
@@ -136,3 +136,5 @@ string1, string2, string3 = '', 'Trondheim', 'Hammer Dance'
 non_null = string1 or string2 or string3
 
 print('non_null', '.' * 5, non_null)
+
+
