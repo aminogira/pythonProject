8b0b0ff5abfb61f7f8a2560ea32cc6cb6e6e84fc
Amila Giragama <aminogira@gmail.com>
2022-11-04 08:46:07 +0530

    3 start

--- hr/hrFive.py
+++ hr/hrFive.py
@@ -1,15 +1,25 @@
 if __name__ == '__main__':
-    nm = []
-    sc = []
+    lst = []
     for _ in range(int(input())):
         name = input()
         score = float(input())
-        nm.append(name)
-        sc.append(score)
+        lst.append([name, score])
+    lst.sort(key=lambda row: (row[1]))
+    min = -1
+    foundSecond = False
+    secondVal = 0
+    secondLowest = []
+    for nm, sc in lst:
+        if min == -1:
+            min = sc
+        if foundSecond == False:
+            if sc != min:
+                foundSecond = True
+                secondVal = sc
+        if sc == secondVal:
+            secondLowest.append(nm)
+    secondLowest.sort()
+    for n in secondLowest:
+        print(n)
 
-    new_list = set(sc)
-    new_list.remove(min(new_list))
-    amt = (min(new_list))
-
-
-//https://www.hackerrank.com/challenges/nested-list/problem?isFullScreen=true
+# https://www.hackerrank.com/challenges/nested-list/problem?isFullScreen=true
