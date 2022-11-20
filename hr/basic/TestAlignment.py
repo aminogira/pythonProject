167aa34f9a6e8e8a9335b5c4a2524c3e64d48a7c
Amila Giragama <aminogira@gmail.com>
2022-11-20 12:16:17 +0530

    day10

--- hr/basic/TestAlignment.py
+++ hr/basic/TestAlignment.py
@@ -15,6 +15,7 @@ for i in range(thickness+1):
 for i in range((thickness+1)//2):
     print((c*thickness*5).center(thickness*6))
 
+
 #Bottom Pillars
 for i in range(thickness+1):
     print((c*thickness).center(thickness*2)+(c*thickness).center(thickness*6))
