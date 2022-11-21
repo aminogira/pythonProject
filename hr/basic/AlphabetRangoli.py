a09e1abf627a67dfcb017c5f5b1fecd805aeb4e3
Amila Giragama <aminogira@gmail.com>
2022-11-21 19:50:00 +0530

    day14

--- hr/basic/AlphabetRangoli.py
+++ hr/basic/AlphabetRangoli.py
@@ -1,6 +1,37 @@
 def print_rangoli(size):
-    # your code goes here
+    print('-'*50)
+    alf = 'abcdefghijklmnopqrstuvwxyz'
+    al = alf[:size][::-1]
+
+    width=(size*2-1)*2-1
+    print(width)
+    for a in al:
+        print(a)
+
+
+
 
 if __name__ == '__main__':
     n = int(input())
     print_rangoli(n)
+
+
+
+
+
+# ----c----
+# --c-b-c--
+# c-b-a-b-c
+# --c-b-c--
+# ----c----
+
+
+# --------e--------
+# ------e-d-e------
+# ----e-d-c-d-e----
+# --e-d-c-b-c-d-e--
+# e-d-c-b-a-b-c-d-e
+# --e-d-c-b-c-d-e--
+# ----e-d-c-d-e----
+# ------e-d-e------
+# --------e--------
